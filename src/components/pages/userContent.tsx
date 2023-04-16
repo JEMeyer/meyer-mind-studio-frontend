1160e490b19351ef914f90137c04e1c96e648e49
Joe Meyer <joseph@meyer.id>
2023-04-16 01:01:53 -0500

    fix passing in useronly flag on user contnet page

--- src/components/pages/userContent.tsx
+++ src/components/pages/userContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Leaderboard from '../organisms/leaderboard';
 
 const UserContent: React.FC = () => {
-    return <Leaderboard onlyUserVideos={false} title='Your Generated Videos' />
+    return <Leaderboard onlyUserVideos={true} title='Your Generated Videos' />
 }
 
 export default UserContent;
