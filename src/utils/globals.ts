1ab54d58b05251b3ea0ef056477cd1aaf83c00b4
Joe Meyer <joseph@meyer.id>
2024-01-15 22:36:47 +0000

    fix image

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,2 +1,2 @@
-export const placeholderImage1 = 'https://img-blog.csdnimg.cn/img_convert/6c674cf08280bd17368749310506481a.png';
+export const placeholderImage1 = 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Astronaut_Riding_a_Horse_%28SDXL%29.jpg';
 export const placeholderImage2 = 'https://upload.wikimedia.org/wikipedia/commons/3/32/A_photograph_of_an_astronaut_riding_a_horse_2022-08-28.png'
